26c6bf1c85c8d9fa4de08579c6190ba948ef6b55
Kristian <10221425@ucn.dk>
2024-10-30 10:48:56 +0100

    Sidebar & Command Component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,13 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Admin Dashboard",
+  title: "Admin dashboard",
   description: "Admin dashboard",
 };
 
@@ -25,12 +17,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang='en'>
+      <body className={inter.className}>
         <Navbar />
-        {children}
+        <div className='flex'>
+          <div className='hidden md:block h-[100vh] w-[300px]'>
+            <Sidebar />
+          </div>
+          <div className='p-5 w-full md:max-w-[1140px]'>{children}</div>
+        </div>
       </body>
     </html>
   );
